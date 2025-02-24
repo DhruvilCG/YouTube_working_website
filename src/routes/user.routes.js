0f97943c43b59a8a461ba4d3ad628406a666970d
Dhruvil <dhruvilpatel.m@gmail.com>
2025-02-24 12:13:53 +0530

    Fix logout functionality to unset refreshToken and update cover image route to use verifyJWT

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -29,7 +29,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account-details").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verify, upload.single("coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
